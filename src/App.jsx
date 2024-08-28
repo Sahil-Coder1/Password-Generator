7ab765c81738ccd7bc1bf68a5bf2dbbfc7a769a4
Sahil-Coder1 <26maysahil@gmail.com>
2024-08-29 00:15:05 +0530

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -42,9 +42,9 @@ if(isNumber && isSymbol){
     <div>
    <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full h-screen' >
    <h1 className='text-4xl py-20 text-center text-white'>Password Generator</h1>
-  <div className='h-90 m-20 text-center content-center'>
+  <div className=' mx-20 text-center content-center p-2 rounded-xl bg-slate-400'>
   <input type="text" name="n" id="pass" value={pass} className='bg-green-200 p-3 rounded-lg text-4xl text-center w-fit' disabled/>
-  <button className='bg-green-600 text-4xl text-white p-3 rounded-xl ml-2 ' onClick={() => {Copy()}}>Copy</button>
+  <button className='bg-green-600 text-4xl text-white p-3 rounded-xl ml-2 hover:bg-green-500 ' onClick={() => {Copy()}}>Copy</button>
   <div>
     <div className='  flex-2 content-center'>
     <label  className='text-orange-800 text-2xl'>Length : {len}</label> &nbsp;
@@ -53,7 +53,7 @@ if(isNumber && isSymbol){
   <input type="checkbox" onChange={(e) => setSymbol(e.target.checked)} className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10" /><label className='text-orange-800 text-2xl '> Symbols</label>
      
     </div>
-  <button className='bg-yellow-600 text-2xl text-white p-4 rounded-xl m-2 ' onClick={() => {Password()}}>Generate</button>
+  <button className='bg-yellow-600 text-2xl text-white p-4 rounded-xl m-2 hover:bg-yellow-500' onClick={() => {Password()}}>Generate</button>
   </div>
   </div>
    </div>
